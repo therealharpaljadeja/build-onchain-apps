82e32174ac8218bee31becb3ceb58d065316613c
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-05-06 15:59:38 +0530

    chore: test navbar with `OnChainProviderWithRK`

--- template/web/src/components/layout/header/Navbar.test.tsx
+++ template/web/src/components/layout/header/Navbar.test.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import { render, screen, waitFor } from '@testing-library/react';
-import OnchainProviders from '@/OnchainProviders';
+import OnchainProviders from '@/OnchainProvidersWithRK';
 import Navbar from './Navbar';
 
 describe('Navbar', () => {
