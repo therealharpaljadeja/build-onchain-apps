4e06addeda27a561039537e0c2576d9f343ba27d
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-05-06 15:57:19 +0530

    chore: trigger actions

--- template/web/app/layout.tsx
+++ template/web/app/layout.tsx
@@ -25,6 +25,8 @@ initAnalytics();
 /** Root layout to define the structure of every page
  * https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts
  */
+
+// Dummy comment
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${inter.className}`}>
